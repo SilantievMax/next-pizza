4624ae19064c0c00a5ca021c2694edb45039bf3e
Silantiev Maksim <silantiievmax@gmail.com>
2024-07-27 11:30:09 +0300

    feat: add seed prisma

---
+++ src/prisma/seed.ts
@@ -0,0 +1,178 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { cartItems, carts, categories, ingredients, pizza1, pizza2, pizza3, products, users } from './constants';
+
+const prisma = new PrismaClient();
+
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const generateProductItem = ({ productId, pizzaType, size }: { productId: number; pizzaType?: number; size?: number }) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    pizzaType,
+    size,
+  } as Prisma.ProductItemUncheckedCreateInput;
+};
+
+async function up() {
+  await prisma.user.createMany({
+    data: users as Prisma.UserCreateManyInput[],
+  });
+
+  await prisma.category.createMany({
+    data: categories,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  await prisma.ingredient.createMany({
+    data: ingredients,
+  });
+
+  const product1 = await prisma.product.create({
+    data: pizza1,
+  });
+
+  const product2 = await prisma.product.create({
+    data: pizza2,
+  });
+
+  const product3 = await prisma.product.create({
+    data: pizza3,
+  });
+
+  await prisma.productItem.createMany({
+    data: [
+      // Пицца "Пепперони фреш"
+      generateProductItem({ productId: product1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: product1.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: product1.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Сырная"
+      generateProductItem({ productId: product2.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: product2.id, pizzaType: 1, size: 30 }),
+      generateProductItem({ productId: product2.id, pizzaType: 1, size: 40 }),
+      generateProductItem({ productId: product2.id, pizzaType: 2, size: 20 }),
+      generateProductItem({ productId: product2.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: product2.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Чоризо фреш"
+      generateProductItem({ productId: product3.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: product3.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: product3.id, pizzaType: 2, size: 40 }),
+
+      // Остальные продукты
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 }),
+    ],
+  });
+
+  await prisma.cart.createMany({
+    data: carts,
+  });
+
+  await prisma.cartItem.create({
+    data: cartItems,
+  });
+
+  // await prisma.story.createMany({
+  //   data: [
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/xep/xzh/zmc/cr4gcw0aselwvf628pbmj3j/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3101815496',
+  //     },
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/km2/9gf/jrn/sb7ls1yj9fe5bwvuwgym73e/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3074015640',
+  //     },
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/quw/acz/zf5/zu37vankpngyccqvgzbohj1/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=1336215020',
+  //     },
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/7oc/5nf/ipn/oznceu2ywv82tdlnpwriyrq/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=38903958',
+  //     },
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/q0t/flg/0ph/xt67uw7kgqe9bag7spwkkyw/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=2941222737',
+  //     },
+  //     {
+  //       previewImageUrl: 'https://cdn.inappstory.ru/story/lza/rsp/2gc/xrar8zdspl4saq4uajmso38/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=4207486284',
+  //     },
+  //   ],
+  // });
+
+  // await prisma.storyItem.createMany({
+  //   data: [
+  //     {
+  //       storyId: 1,
+  //       sourceUrl: 'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
+  //     },
+  //     {
+  //       storyId: 1,
+  //       sourceUrl: 'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
+  //     },
+  //     {
+  //       storyId: 1,
+  //       sourceUrl: 'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
+  //     },
+  //     {
+  //       storyId: 1,
+  //       sourceUrl: 'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
+  //     },
+  //     {
+  //       storyId: 1,
+  //       sourceUrl: 'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
+  //     },
+  //   ],
+  // });
+}
+
+async function down() {
+  // const tables = ['User', 'Product', 'Ingredient', 'Order', 'ProductItem', 'VerificationCode', 'Category', 'Cart', 'CartItem', 'Story', 'StoryItem'];
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "Order" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "VerificationCode" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "Story" RESTART IDENTITY CASCADE;`;
+  // await prisma.$executeRaw`TRUNCATE TABLE "StoryItem" RESTART IDENTITY CASCADE;`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
